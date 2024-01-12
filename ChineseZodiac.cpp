e4abeff13993d42bc7dfc7add49c1343a0198a14
BirdsArentRea1 <146360250+BirdsArentRea1@users.noreply.github.com>
2024-01-12 10:51:10 -0700

    Create ChineseZodiac.cpp

---
+++ ChineseZodiac.cpp
@@ -0,0 +1,27 @@
+import math //this code runs fine for me but when I submit it to codequest academy it syas it has a runtime error
+cases = int(input().rstrip())
+
+for i in range(cases):
+    line = int(input().rstrip())
+    
+    print(line, end = " ")
+    
+    #yang yin
+    if line%2==0:
+        print("Yang", end = " ")
+    else:
+        print("Yin", end = " ")
+        
+    #element
+    elements = ["Wood", "Fire", "Metal", "Water", "Earth"]
+    element = line - 4
+    element = element%10
+    element = math.floor(element/2)
+    print(elements[element], end = " ")
+    
+    #animal
+    animals = ["Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake" "Horse", "Goat", "Monkey", "Rooster", "Dog", "Pig"]
+    year = line - 4
+    year = year%12
+    print(animals[year], end = " ")
+        
